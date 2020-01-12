6c74f94d409c334c17dd541eb648f61293619f0a
bharat bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-01-12 17:09:32 +0530

    setup static files layout and pages

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express =require('express');
 const app=express();
 const port=8000;
-//use express  router
+
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+//use express  router as routes folder
 app.use('/',require('./routes'));
+
+
+//set up the view engine c
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`error occurs in ${port}`);
